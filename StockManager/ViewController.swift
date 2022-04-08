5cfa64ab9ae80b34cefea9d242556d9143e09a6b
Chavalit Vanasapdamrong <chavalit.v@siriustech.io>
2022-04-08 23:31:19 +0700

    Initial Commit

---
+++ StockManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockManager
+//
+//  Created by Chavalit Vanasapdamrong on 8/4/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
